25f423769f4ddf5c2e1f1c2f6c93d8c22268ff2b
lanyu <lanyu@lanyudeMacBook-Pro-7.local>
2022-03-15 11:07:12 +0800

    add proxy initializedProxy func

--- contracts/proxy/OwnedUpgradeabilityProxy.sol
+++ contracts/proxy/OwnedUpgradeabilityProxy.sol
@@ -25,6 +25,14 @@ contract OwnedUpgradeabilityProxy {
     bytes32 private constant proxyOwnerPosition = 0x6279e8199720cf3557ecd8b58d667c8edc486bd1cf3ad59ea9ebdfcae0d0dfac; //keccak256("trueUSD.proxy.owner");
     bytes32 private constant pendingProxyOwnerPosition = 0x8ddbac328deee8d986ec3a7b933a196f96986cb4ee030d86cc56431c728b83f4; //keccak256("trueUSD.pending.proxy.owner");
 
+    bool public initializedProxy;
+    function init() public {
+        require(!initializedProxy, "already initialized");
+        _setUpgradeabilityOwner(msg.sender);
+        initializedProxy = true;
+    }
+
+
     /**
      * @dev the constructor sets the original owner of the contract to the sender account.
      */
