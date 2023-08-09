02ebff34017578795b61a3e7c91d1262670497f4
qiwihui <qwh005007@gmail.com>
2023-08-09 16:08:51 +0800

    🔗 Fix lint for tron contracts (#4)

--- packages/contracts-tron/contracts/TronTokenController.sol
+++ packages/contracts-tron/contracts/TronTokenController.sol
@@ -106,7 +106,6 @@ contract TokenController {
         _;
     }
 
-
     modifier onlyRegistryAdmin() {
         require(msg.sender == registryAdmin || msg.sender == owner, "must be registry admin or owner");
         _;
@@ -190,9 +189,9 @@ contract TokenController {
     }
 
     /**
-    * @dev sets the original `owner` of the contract to the sender
-    * at construction. Must then be reinitialized 
-    */
+     * @dev sets the original `owner` of the contract to the sender
+     * at construction. Must then be reinitialized
+     */
     constructor() public {
         owner = msg.sender;
         emit OwnershipTransferred(address(0), owner);
@@ -206,11 +205,19 @@ contract TokenController {
         instantMintThreshold = 150_000_000_000_000_000_000_000_000;
         ratifiedMintThreshold = 300_000_000_000_000_000_000_000_000;
         multiSigMintThreshold = 1_000_000_000_000_000_000_000_000_000;
-        emit MintThresholdChanged(150_000_000_000_000_000_000_000_000, 300_000_000_000_000_000_000_000_000, 1_000_000_000_000_000_000_000_000_000);
+        emit MintThresholdChanged(
+            150_000_000_000_000_000_000_000_000,
+            300_000_000_000_000_000_000_000_000,
+            1_000_000_000_000_000_000_000_000_000
+        );
         instantMintLimit = 150_000_000_000_000_000_000_000_000;
         ratifiedMintLimit = 300_000_000_000_000_000_000_000_000;
         multiSigMintLimit = 1_000_000_000_000_000_000_000_000_000;
-        emit MintLimitsChanged(150_000_000_000_000_000_000_000_000, 300_000_000_000_000_000_000_000_000, 1_000_000_000_000_000_000_000_000_000);
+        emit MintLimitsChanged(
+            150_000_000_000_000_000_000_000_000,
+            300_000_000_000_000_000_000_000_000,
+            1_000_000_000_000_000_000_000_000_000
+        );
 
         initialized = true;
     }
@@ -481,7 +488,6 @@ contract TokenController {
         mintKey = _newMintKey;
     }
 
-
     function setRegistryAdmin(address admin) external onlyOwner {
         registryAdmin = admin;
     }
@@ -644,5 +650,4 @@ contract TokenController {
     function setBlacklisted(address account, bool isBlacklisted) external onlyRegistryAdmin {
         token.setBlacklisted(account, isBlacklisted);
     }
-
 }
